48de0742cb41fa1c1d385db50dfe4f17bb6f8814
TJahongir <jakhongirt@gmail.com>
2022-03-13 01:39:10 +0500

    git ignore

---
+++ src/components/Category.jsx
@@ -0,0 +1,11 @@
+import {FaPizzaSlice, FaHamburger} from 'react-icons/fa'
+import {GiNoodles, GiChopSticks} from 'react-icons/gi' 
+import styled from 'styled-components'
+
+const Category = () => {
+  return (
+    <div>Category</div>
+  )
+}
+
+export default Category
