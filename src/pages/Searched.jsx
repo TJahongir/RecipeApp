661f5c31985d7d02119d1b3da330c891a7783fdd
TJahongir <jakhongirt@gmail.com>
2022-12-21 09:56:17 +0500

    Search Bar is done

---
+++ src/pages/Searched.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import {Grid, Card} from '../components/styled'
+
+function Searched() {
+
+    const [searchedRecipes, setSearchedRecipes] = useState("")
+    let params = useParams()
+
+    useEffect(() => {
+        getSearched(params.query)
+    }, [params.query])
+
+    const getSearched = async (name) => {
+        const data = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${import.meta.env.VITE_APP_API_KEY}&query=${name}&number=9`)
+        const recipes = await data.json();
+        let results = recipes.results
+        setSearchedRecipes(results)
+    }
+
+
+
+  return (
+    <Grid>
+        {searchedRecipes.map((item) => {
+            return (
+                <Card key={item.id}>
+                    <img src={item.image} alt={item.title} />
+                    <h4>{item.title}</h4>
+                </Card>
+            )
+        })}
+    </Grid>
+  )
+}
+
+export default Searched
