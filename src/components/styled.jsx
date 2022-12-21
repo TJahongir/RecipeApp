fcd728c77f8599890bc5a11123589f264c16568a
TJahongir <jakhongirt@gmail.com>
2022-12-21 08:56:58 +0500

    Cuisines added

--- src/components/styled.jsx
+++ src/components/styled.jsx
@@ -53,4 +53,27 @@ const List = styled.div`
     margin: 2rem 0rem;
 
 `
-export { Wrapper, Gradient, Card, List} 
+
+const Grid = styled.div`
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
+    grid-gap: 3rem;
+`
+
+const CCard = styled.div`
+    img {
+        width: 100%;
+        border-radius: 2rem;
+    }
+    a {
+        text-decoration: none;
+    }
+    h4 {
+        text-align: center;
+        padding: 1rem;
+    }
+
+`
+
+
+export { Wrapper, Gradient, Card, List, Grid, CCard} 
--- src/pages/Cuisine.jsx
+++ src/pages/Cuisine.jsx
@@ -1,6 +1,36 @@
+import {Grid, CCard} from '../components/styled'
+import {motion} from 'framer-motion'
+import {Link, useParams} from 'react-router-dom'
+import {useState, useEffect} from 'react'
+
 function Cuisine() {
+    const [cuisine, setCuisine] = useState([])
+    let params = useParams();
+
+
+    useEffect(() => {
+        getCuisine(params.type)
+    }, [params.type])
+
+    const getCuisine = async (name) => {
+        const data = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${import.meta.env.VITE_APP_API_KEY}&cuisine=${name}&number=9`)
+        const recipes = await data.json();
+
+        setCuisine(recipes.results)
+    }
+
+
   return (
-    <div>Cuisine</div>
+    <Grid>
+        {cuisine.map((item) => {
+            return(
+                <CCard key={item.id}>
+                    <img src={item.image} alt={item.title}/>
+                    <h4>{item.title}</h4>
+                </CCard>
+            )
+        })}
+    </Grid>
   )
 }
 
